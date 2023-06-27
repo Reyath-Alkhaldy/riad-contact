6166ef3581067d18bb25ad3c5065827e082d0ee6
RiyadAlkhaldy <124738245+RiyadAlkhaldy@users.noreply.github.com>
2023-06-27 19:51:56 +0300

    add version 111

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'riyad/contact-us',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8238c573ed982737e0f88d69f8ff6f85691695d6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'riyad/contact-us' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8238c573ed982737e0f88d69f8ff6f85691695d6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
